c69b164d63ae118f66c3193ad905922dd91f647c
Hugo Sousa <hugosousa42@proton.me>
2025-01-24 21:31:41 +0000

    Initial Structure.

---
+++ SoccerLeagueWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoccerLeagueWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
